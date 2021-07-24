b5c71d27fc7941ff680c41720df3f3cb246e4a85
siddhary <sid.infinity.yadav@gmail.com>
2021-07-24 23:42:10 +0530

    gh

--- src/main/java/com/example/googledemo/SecurityConfig.java
+++ src/main/java/com/example/googledemo/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.googledemo;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -15,7 +17,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.cors().and().csrf().disable();
+        http.cors().and().csrf().disable()
+                .authorizeRequests(a -> a
+                        .antMatchers("/api/**", "/error", "/webjars/**").permitAll()
+                        .antMatchers("/", "/index.html").authenticated()
+                        .anyRequest().authenticated()
+                ) .oauth2Login().permitAll()
+                .and().logout().logoutSuccessUrl("/");
+
+
     }
 
     @Bean
