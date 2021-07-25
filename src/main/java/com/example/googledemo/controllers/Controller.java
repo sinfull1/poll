4c52948a48589a4c57e91231d591a4fd7db0ae84
siddhary <sid.infinity.yadav@gmail.com>
2021-07-25 09:46:07 +0530

    fix

--- src/main/java/com/example/googledemo/controllers/Controller.java
+++ src/main/java/com/example/googledemo/controllers/Controller.java
@@ -9,6 +9,8 @@ import com.example.googledemo.repository.CommentRepository;
 import com.example.googledemo.repository.PollsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -39,7 +41,7 @@ public class Controller {
     @GetMapping("/api/bid/{pollkey}/{side}")
     public Mono<Bid> bid(@PathVariable String pollkey, @PathVariable("side") String side,@AuthenticationPrincipal Principal principal ) {
         Bid bid = new Bid();
-        bid.setUsername(principal==null?"User":principal.getName());
+        bid.setUsername(principal==null?"User":((OAuth2AuthenticationToken) principal).getPrincipal().getAttribute("name"));
         bid.setPollkey(pollkey);
         bid.setSide(Boolean.parseBoolean(side));
         bid.setAmount(0.0);
